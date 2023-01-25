7c66fe4b940abbb1814591f832a45a476713a18b
Voxel20 <aidensmith20@gmail.com>
2023-01-24 23:44:36 -0500

    Initial Commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Nextra Docs</span>,
+  project: {
+    link: "",
+  },
+  chat: {
+    link: "",
+  },
+  docsRepositoryBase: "",
+  footer: {
+    text: "Nextra Docs",
+  },
+};
+
+export default config;
