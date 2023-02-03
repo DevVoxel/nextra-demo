9f491bf86b9ddbea2132da5f0b058e5203024a85
Voxel20 <aidensmith20@gmail.com>
2023-02-02 22:11:03 -0500

    Naming Change

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Nextra Docs</span>,
+  logo: <span>Demo Docs</span>,
   project: {
     link: "https://github.com/Voxel20/nextra-demo.git",
   },
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: "https://github.com/Voxel20/nextra-demo.git/docs",
   footer: {
-    text: "Nextra Docs",
+    text: "Demo Docs",
   },
 };
 
